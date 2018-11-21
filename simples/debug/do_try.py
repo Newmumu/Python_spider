fd40507c65f4167f30d42ea82a59130ebacc25b1
linlin <15574412169@163.com>
2018-11-21 13:48:18 +0800

    add file debug/do_try.py

---
+++ simples/debug/do_try.py
@@ -0,0 +1,13 @@
+# encoding=utf-8
+try:
+    print('try...')
+    r = 10 / 0
+    print('result:', r)
+
+except ZeroDivisionError as e:
+    print('except:', e)
+
+finally:
+    print('finally...')
+
+print('END')
