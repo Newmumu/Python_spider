30087bae0a3539c4bd27876e5b94e034c0c87f40
linlin <15574412169@163.com>
2018-11-22 10:07:43 +0800

    add 2 files simples/basic/do_input.py + do_while.py

---
+++ simples/basic/do_input.py
@@ -0,0 +1,4 @@
+# encoding=utf-8
+
+name = raw_input()
+print 'hello,',name, 'come on ,you will be ok!'
---
+++ simples/basic/do_while.py
@@ -0,0 +1,13 @@
+# encoding=utf-8
+
+sum = 0
+n = 1
+while n < 100:
+    sum = sum + n
+print sum
+
+sum2 = 1
+n = 1
+while n < 100:
+    sum2 = sum2 * n
+print sum2
