5667d3d5a96e7e423bd693ed04292e38f5f05e6b
fairylin <15574412169@163.com>
2020-10-20 23:29:25 +0800

    修改gitignore文件,更新源文件格式

--- Python3网络编程/client.py
+++ Python3网络编程/client.py
@@ -9,17 +9,15 @@ host = socket.gethostname()
 port = 9999
 # 客户端尝试指定主机地址及端口进行网络连接
 clientsocket.connect((host, port))
-while True:	
-	# 接收从服务器返回的数据（每次接收1024个字节的数据），并打印
-	msg_r = clientsocket.recv(1024)
-	print("服务器：", msg_r.decode('utf-8'))
-	
-	# 定义发送的文本信息到服务端
-	msg_s = input('客户端：')
-	# 客户端发送数据到服务端
-	clientsocket.send(msg_s.encode('utf-8'))
-	if msg_s == 'q':
-		clientsocket.close()
-		break
+while True:
+    # 接收从服务器返回的数据（每次接收1024个字节的数据），并打印
+    msg_r = clientsocket.recv(1024)
+    print("服务器:", msg_r.decode('utf-8'))
 
-	
+    # 定义发送的文本信息到服务端
+    msg_s = input('客户端:')
+    # 客户端发送数据到服务端
+    clientsocket.send(msg_s.encode('utf-8'))
+    if msg_s == 'q':
+        clientsocket.close()
+        break
--- Python3网络编程/server.py
+++ Python3网络编程/server.py
@@ -1,5 +1,6 @@
 # encoding:utf-8
 import socket
+
 # 定义服务器套接字
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 获取本地主机地址
@@ -14,27 +15,27 @@ serversocket.bind((host, port))
 serversocket.listen(5)
 
 while True:
-	# 接受客户端的连接，将连接对象及远程客户端地址分别保存在变量中
-	conn, addr = serversocket.accept()
-	# 输出远程客户端地址（明确其身份）
-	print('连接地址:%s' %str(addr))
-	# 输出欢迎语句
-	msg_s = "欢迎访问的socket服务器！"
-	conn.send(msg_s.encode('utf-8'))
-	while True:
-		# 接收从客户端发送来的消息，并输出到客户端
-		msg_r = conn.recv(1024)
-		print("客户端：%s" %msg_r.decode('utf-8'))
-		if msg_r == 'q':
-			conn.close()
-			break
+    # 接受客户端的连接，将连接对象及远程客户端地址分别保存在变量中
+    conn, addr = serversocket.accept()
+    # 输出远程客户端地址（明确其身份）
+    print('连接地址:%s' % str(addr))
+    # 输出欢迎语句
+    msg_s = "欢迎访问的socket服务器！"
+    conn.send(msg_s.encode('utf-8'))
+    while True:
+        # 接收从客户端发送来的消息，并输出到客户端
+        msg_r = conn.recv(1024)
+        print("客户端:%s" % msg_r.decode('utf-8'))
+        if msg_r == 'q':
+            conn.close()
+            break
 
-		# 定义服务器返回的数据，并发送回连接状态的客户端
-		msg_s2 = input("服务器：")
-		conn.send(msg_s2.encode('utf-8'))
-		if msg_s2 == 'q':
-			conn.close()
-			break
-	break
+        # 定义服务器返回的数据，并发送回连接状态的客户端
+        msg_s2 = input("服务器:")
+        conn.send(msg_s2.encode('utf-8'))
+        if msg_s2 == 'q':
+            conn.close()
+            break
+    break
 
 serversocket.close()
